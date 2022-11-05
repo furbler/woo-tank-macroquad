b1816e85879dcc481c442033fba3d018cafc84a3
furbler <63544661+furbler@users.noreply.github.com>
2022-11-06 01:07:51 +0900

    左右方向キーで砲塔旋回

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use macroquad::prelude::*;
-
+/// 角度はすべて-y軸方向を0、時計回りを正とする
 struct Turret {
-    rot: f32,           // 車体を基準とした砲塔の角度
-    angular_speed: f32, // 砲塔の旋回速度
+    rot: f32,           // 車体を基準とした砲塔の角度[deg]
+    angular_speed: f32, // 砲塔の旋回速度[deg]
     texture: Texture2D,
     image_scale: f32, // 画像描画時の拡大率
 }
 
-struct Tank {
+struct Body {
     pos: Vec2,          // 車体中心位置
-    rot: f32,           // 前方向を表す角度[deg] -y方向が0、時計回りが正とする
+    rot: f32,           // 前方向を表す角度[deg]
     speed: i32,         // 1フレームあたりの移動距離
     angular_speed: f32, // 1フレームあたりの旋回速度
     texture: Texture2D,
@@ -19,7 +19,7 @@ struct Tank {
 
 #[macroquad::main("woo-tank-macroquad")]
 async fn main() {
-    let mut player = Tank {
+    let mut player = Body {
         pos: Vec2::new(screen_width() / 2., screen_height() / 2.),
         rot: 20.,
         speed: 5,
@@ -35,29 +35,43 @@ async fn main() {
     };
 
     loop {
-        // 右旋回
-        if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+        // 車体右旋回
+        if is_key_down(KeyCode::D) {
             player.rot += player.angular_speed;
         }
 
-        // 左旋回
-        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+        // 車体左旋回
+        if is_key_down(KeyCode::A) {
             player.rot -= player.angular_speed;
         }
+        // 1周したら戻す
+        player.rot %= 360.;
 
-        let rot_rad = player.rot.to_radians();
+        // 砲塔右旋回
+        if is_key_down(KeyCode::Right) {
+            player.turret.rot += player.turret.angular_speed;
+        }
+
+        // 砲塔左旋回
+        if is_key_down(KeyCode::Left) {
+            player.turret.rot -= player.turret.angular_speed;
+        }
+        // 1周したら戻す
+        player.turret.rot %= 360.;
+
+        let body_rot_rad = player.rot.to_radians();
         // 速度(移動量)
         let mut vel = Vec2::new(0., 0.);
 
         // 前進
-        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+        if is_key_down(KeyCode::W) {
             // y軸に反転
-            vel += Vec2::new(rot_rad.sin(), -rot_rad.cos()) * player.speed as f32;
+            vel += Vec2::new(body_rot_rad.sin(), -body_rot_rad.cos()) * player.speed as f32;
         }
         // 後進
-        if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+        if is_key_down(KeyCode::S) {
             // y軸に反転
-            vel -= Vec2::new(rot_rad.sin(), -rot_rad.cos()) * player.speed as f32;
+            vel -= Vec2::new(body_rot_rad.sin(), -body_rot_rad.cos()) * player.speed as f32;
         }
         player.pos += vel;
 
@@ -73,7 +87,7 @@ async fn main() {
                     player.texture.width() * player.image_scale,
                     player.texture.height() * player.image_scale,
                 )),
-                rotation: rot_rad,
+                rotation: body_rot_rad,
                 ..Default::default()
             },
         );
@@ -88,7 +102,7 @@ async fn main() {
                     player.turret.texture.width() * player.turret.image_scale,
                     player.turret.texture.height() * player.turret.image_scale,
                 )),
-                rotation: rot_rad,
+                rotation: body_rot_rad + player.turret.rot.to_radians(),
                 ..Default::default()
             },
         );
